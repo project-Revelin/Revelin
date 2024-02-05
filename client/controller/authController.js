571993fc487a4770e2fb745445fcff32057d70da
Gurdeep Singh <gurdeepsingh8975@gmail.com>
2024-02-05 12:26:04 +0530

    done

--- client/controller/authController.js
+++
@@ -1,9 +0,0 @@
-const registerController = async () =>{
-    try {
-        console.log('Auth controller')
-    } catch (error) {
-        console.log(error)
-    }
-}
-
-module.exports = {registerController}
